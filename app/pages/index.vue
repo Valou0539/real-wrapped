6925bf4a5f95852b19d9801a89871bc0177edad0
Valou0539 <valentin.gindre@gmail.com>
2025-11-21 23:49:46 +0100

    chore: update landing page and global translations

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,14 @@
 <template>
-  <main>
-    <div class="size-96"></div>
+  <main class="mx-auto mt-20 max-w-4xl px-4 py-16">
+    <ClientOnly>
+      <HistoryUpload v-if="!isWrapped" />
+      <WrappedStats v-else />
+    </ClientOnly>
   </main>
 </template>
 <script lang="ts" setup>
+const { isWrapped } = storeToRefs(useWrappedStore());
+
 useSeoMeta({
   title: $t("home.seo.title"),
   description: $t("home.seo.description"),
