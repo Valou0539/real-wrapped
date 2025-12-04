3519e53dfcc826f734c05483d3f4c437a0de9bfb
Valou0539 <valentin.gindre@gmail.com>
2025-12-04 13:52:04 +0100

    style: add full width constraint to main container on index page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="mx-auto mt-20 max-w-4xl px-4 py-16">
+  <main class="mx-auto mt-20 w-full max-w-4xl px-4 py-16">
     <ClientOnly>
       <HistoryUpload v-if="!isWrapped" />
       <WrappedStats v-else />
